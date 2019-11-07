610eaae190a16b8a93f42b6ebfad9d241033458f
kyrasegraves <ksegraves@smu.edu>
2019-11-06 23:05:15 -0600

    working on making grammar work

--- main.js
+++ main.js
@@ -1,6 +1,12 @@
 fs = require("file-system");
 peg = require("pegjs");
 
+const codeExample = `let a = 99\n
+  		let f = fn(x) { x + a }\n
+  		print(f(1))     #=> 100\n
+  		a = 100\n
+  		print(f(1))     #=> 101\n`;
+
 fs.readFile("grammar.txt", "utf8", function (err, data) {
 	if (err) {
 		throw err;
@@ -9,6 +15,10 @@ fs.readFile("grammar.txt", "utf8", function (err, data) {
 	
 	const grammar = data;
 	const parser = peg.generate(grammar);
+	const ast = parser.parse(codeExample);
+	fs.writeFile("ast.txt", JSON.stringify(ast), function(err) {
+		if (err) {console.log(err);}
+	})
 })
 
 // run w/	node main.js
