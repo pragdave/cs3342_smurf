3223c9146467a748dcc7f993699f118eaaa29bbf
kyrasegraves <ksegraves@smu.edu>
2019-11-11 19:11:19 -0600

    execute multiple print statements :D

--- main.js
+++ main.js
@@ -4,14 +4,12 @@ peg = require("pegjs");
 
 function printFunction(valArr) { console.log(valArr[0]); }
 
-function generateASTNodes(statement) {
-	// iterate through generated ast and create AST nodes
-	// ast.forEach(statement => {
+function generateNode(statement) {
 	if (statement.type === "function") {
 		const body = statement.fcn_name === "print" ? printFunction : statement.body;
-			// TODO: generateASTNodes for statement.body
+			// TODO: generateNode for statement.body
 		const newNode = new treeNodes.FunctionNode(statement.fcn_name);
-		const params = statement.params.map(p => generateASTNodes(p)).filter(p => !!p);
+		const params = statement.params.map(p => generateNode(p)).filter(p => !!p);
 		const paramsNode = new treeNodes.ParamsNode(params);
 		newNode.setParams(paramsNode);
 		newNode.setBody(body);
@@ -20,16 +18,25 @@ function generateASTNodes(statement) {
 		const newNode = new treeNodes.ValueNode(statement.value);
 		return newNode;
 	}
-	// })
 }
 
-function executeAST(astNode) {
+function generateASTNodes(ast) {
+	return ast.map(statement => generateNode(statement));
+}
+
+function executeNode(astNode) {
 	if (astNode.type === "function") {
 		astNode.executeBody();
 	}
 }
 
-const codeExample = "print(1)";
+function executeAST(ast) {
+	ast.forEach(node => executeNode(node));
+}
+
+const codeExample = `print(1)\n` +
+					`print(2)`;
+
 // const codeExample = `let a = 99\n
 //   		let f = fn(x) { x + a }\n
 //   		print(f(1))     #=> 100\n
@@ -45,11 +52,11 @@ fs.readFile("grammar.txt", "utf8", function (err, data) {
 	const grammar = data;
 	const parser = peg.generate(grammar);
 	const ast = parser.parse(codeExample);
-	fs.writeFile("ast.txt", JSON.stringify(ast), function(err) {
+	fs.writeFile("ast.txt", JSON.stringify(ast, null, "\t"), function(err) {
 		if (err) {console.log(err);}
 	})
-	const newAST = generateASTNodes(ast[0]);
-	executeAST(newAST);
+	const astArr = generateASTNodes(ast);
+	executeAST(astArr)
 });
 
-// run w/	node main.js
+// run w/ node main.js
