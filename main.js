00e780c1e743407dc3c2899f09d70ebc4c5c7894
kyrasegraves <ksegraves@smu.edu>
2019-11-15 22:50:43 -0600

    comments working, hacky, but working

--- main.js
+++ main.js
@@ -76,42 +76,52 @@ function executeAST(rootNode) {
 	rootNode.statements.forEach(s => execution.ExecuteStatement(s));
 }
 
-// const codeExample = "let a = 1\nprint(1)\nlet b = 4\nprint(a+b)";
+// const codeExample = 
+// "print(1)            #=> 1\n" +
+// "print(3 - 1)        #=> 2\n" +
+// "print(2+1)          #=> 3\n" +
+// "print(2 * 5 - 3*2)  #=> 4\n" +
+// "print(4 - -1)       #=> 5\n" +
+// "print(5--1)         #=> 6\n" +
+// "print(21/3)         #=> 7\n" +
+// "print((3-1)*(3+1))  #=> 8\n";
 
 // const codeExample =
-// "let a = 3\n" +
-// "let b = 4\n\n" +
-// "print(b-a)\n" +		// 1
-// "print(a-1)\n" +		// 2
-// "print(2+b/a)\n" +		// 3
-// "print(b)\n\n" +		// 4
-// "a = a + b\n" +
-// "b = b-a\n\n" +
-// "print(a)\n" +			// 7
-// "print(b)\n\n" +		// -3
-// "let c = a\n" +
-// "a = b\n" +
-// "b = c\n\n" +
-// "print(a)\n" +			// -3
-// "print(b)\n"			// 7
+// `let a = 3\n
+// let b = 4\n\n
+// print(b-a)          #=> 1\n
+// print(a-1)          #=> 2\n
+// print(2+b/a)        #=> 3\n
+// print(b)            #=> 4\n\n
+// a = a + b\n
+// b = b-a\n\n
+// print(a)            #=> 7\n
+// print(b)            #=> -3\n\n
+// let c = a\n
+// a = b\n
+// b = c\n\n
+// print(a)            #=> -3\n
+// print(b)            #=> 7\n`;
 
 const codeExample = 
-"let a = 1\n" +
-"print(a)\n" + 			// 1
-"let b = a+2\n" + 
-"print(b)\n" + 			// 3
-"let c = a+b\n" +
-"print(c)\n\n" +		// 4
-// multiple declarations in a single let
-"let e = 99, f = 100, g = e+f\n" +
-"print(e,f,g)\n\n" +	// 99|100|199
-// on multiple lines
-"let h = 99,\n" +
-"\ti = 200,\n" +
-"\tj = h+i\n" +
-"print(h,i,j)\n"		// 99|200|299
+`let a = 1\n
+print(a)          #=> 1\n
+let b = a+2\n
+print(b)          #=> 3\n
+let c = a+b\n
+print(c)          #=> 4\n\n
 
+# multiple declarations in a single let\n\n
 
+let e = 99, f = 100, g = e+f\n
+print(e,f,g)   #=> 99|100|199\n\n
+
+# on multiple lines\n\n
+
+let h = 99,\n
+    i = 200,\n
+    j = h+i\n
+print(h,i,j)   #=> 99|200|299\n`;
 
 fs.readFile("grammar.txt", "utf8", function (err, data) {
 	if (err) {
