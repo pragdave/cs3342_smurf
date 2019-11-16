46058b234323733927f57d553038ca48e5e108a0
jackrosenblatt <jdrosenblatt18@gmail.com>
2019-11-16 15:33:09 -0600

    Added boolean expression functionality

--- ASTGenerator.py
+++ ASTGenerator.py
@@ -1,26 +1,29 @@
 from arpeggio import PTNodeVisitor
-from AstNodes import Number, Factor, Term, Arithmetic_Expression, Print, Code
+import AstNodes as nodes
 
 class ASTGenerator(PTNodeVisitor):
     def visit_number(self, node, children):
-        return Number(node.value)
+        return nodes.Number(node.value)
     
     def visit_factor(self, node, children):
         if len(children) == 1:
-            return Factor(children[0], "none")
+            return nodes.Factor(children[0], "none")
         if children[0] == '-':
-            return Factor(children[1], "-")
+            return nodes.Factor(children[1], "-")
         else:
-            return Factor(children[1], "+")
+            return nodes.Factor(children[1], "+")
         
     def visit_term(self, node, children):
-        return Term(children[0], children[1:])
+        return nodes.Term(children[0], children[1:])
         
     def visit_arithmetic_expression(self, node, children):
-        return Arithmetic_Expression(children[0], children[1:])
+        return nodes.Arithmetic_Expression(children[0], children[1:])
         
+    def visit_boolean_expression(self, node, children):
+        return nodes.Boolean_Expression(children[0], children[1], children[2])
+    
     def visit_print_func(self, node, children):
-        return Print(children[0])
+        return nodes.Print(children[0])
         
     def visit_code(self, node, children):
-        return Code(children)
+        return nodes.Code(children)
