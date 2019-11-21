ca027b4a011c03af8e5660a7f386ae79bfc19cd7
TroyRWalker <35703217+TroyRWalker@users.noreply.github.com>
2019-11-20 21:00:50 -0600

    Fixed some bugs with implementing the grammar. Got the parse tree to generate and print correctly

--- grammar.py
+++ grammar.py
@@ -11,13 +11,13 @@ def comment(): return "#", _(r'.*')
 
 def code() : return ZeroOrMore(statement)
 
-def statement() : return ("let", variable_declaration) | assignment | expr
+def statement() : return [("let", variable_declaration) , assignment , expr]
           
 def variable_declaration(): return decl, ZeroOrMore(",", decl)
 
 def decl(): return identifier, Optional("=", expr)
 
-def identifier() : return [a-z], ZeroOrMore([a-zA-Z_0-9])
+def identifier() : return [_(r'[a-z]')], ZeroOrMore([_(r'\w+')])
 
 def variable_reference(): return identifier
 
@@ -25,31 +25,32 @@ def if_expression(): return expr, brace_block, Optional( "else", brace_block )
 
 def assignment(): return identifier, "=", expr
 
-def expr(): return "fn", function_definition | "if", if_expression | boolean_expression | arithmetic_expression
+def expr(): return [("fn", function_definition), ("if", if_expression) , boolean_expression ,arithmetic_expression]
 
 def boolean_expression(): return arithmetic_expression, relop, arithmetic_expression
 
-def arithmetic_expression(): return mult_term, addop, arithmetic_expression | mult_term
+def arithmetic_expression(): return [(mult_term, addop, arithmetic_expression) , mult_term]
 
-def mult_term(): return (primary, mulop, mult_term) | primary
+def mult_term(): return [(primary, mulop, mult_term) , primary]
 
-def primary():  return integer | function_call | variable_reference | "(", arithmetic_expression, ")"
-
-
-def integer(): return Optional("-"), OneOrMore[0-9]
-
-def addop(): return '+' | '-'
-def mulop(): return '*' | '/'
-def relop(): return '==' | '!=' | '>=' | '>' | '<=' | '<'
+def primary():  return [integer , function_call , variable_reference , ( "(", arithmetic_expression, ")")]
 
 
-def function_call(): return variable_reference, "(", call_arguments, ")" | "print", "(", call_arguments, ")"
+def integer(): return Optional("-"), OneOrMore(_(r'\d+'))
+#def integer(): return Optional("-"), OneOrMore(_(r'[0-9]'))
+
+def addop(): return ['+',  '-']
+def mulop(): return ['*' , '/']
+def relop(): return ['==' , '!=' , '>=' , '>' , '<=' , '<']
+
+
+def function_call(): return [(variable_reference, "(", call_arguments, ")") , ("print", "(", call_arguments, ")")]
 
 def call_arguments(): return Optional(expr, ZeroOrMore(",", expr))
 
 def function_definition(): return param_list, brace_block
 
-def param_list():  return ("(", identifier, ZeroOrMore(",", identifier), ")") |  "(" ")"
+def param_list():  return [("(", identifier, ZeroOrMore(",", identifier), ")") ,  "(" ")"]
 
 def brace_block(): return "{", code, "}"
 
@@ -58,8 +59,8 @@ def main(debug = False):
     #test_program = open(os.path.join(current_dir, 'C:\\Users\\mailt\Documents\\GitHub\\cs3342_smurf\\test_cases\\00_expr.smu')).read()
     parser = ParserPython(program, comment, debug = debug)
     #parse_tree = parser_parse(test_program)
-    parse_tree = parser_parse("1")
-    print(parse_tree)
+    parser_tree = parser.parse("1 + (2 * 3)")
+    print(parser_tree)
 
 if __name__ == "__main__":
     main(debug=True)
--- token.py
+++ token.py
@@ -4,6 +4,12 @@ class Token:
     token_minus = 'token_minus'
     token_mult = 'token_mult'
     token_divide = 'token_divide'
+    token_equals = 'token_equals'
+    token_notEquals = 'token_notEquals'
+    token_greaterThenOrEquals = 'token_greaterThenOrEquals'
+    token_greaterThen = 'token_lessThen'
+    token_lessThenOrEquals = 'token_lessThenOrEquals'
+    token_lessThen = 'token_lessThen'
     token_lParenthesis = 'token_lParenthesis'
     token_rParenthesis = 'token_rParenthesis'
     def __init__(self, tokenType, value):
@@ -12,4 +18,5 @@ class Token:
     def __repr__(self):
         if self.value: return f'{self.tokenType}:{self.value}'
         return f'{self.tokenType}'
+    
         
