108030f91fd353cd80ad505ef3bf1f4df22f35d2
Jaymie Ruddock <jruddock@smu.edu>
2019-12-08 18:49:59 -0600

    working on interpreting

--- tree_nodes.py
+++ tree_nodes.py
@@ -1,10 +1,127 @@
 
-class integer():
+
+
+class Declaration:
+    def __init__(self, ident, value):
+        self.ident = ident
+        self.value = value
     
+    def evaluate(self, binding):
+        return self.ident.evaluate(binding)
+
+#######
+# OPS #
+#######
+
+class Times:
+    def __init__(self, lhs, rhs):
+        self.lhs = lhs
+        self.rhs = rhs
+    
+    def evaluate(self, binding):
+        res = self.lhs.evaluate(binding) * self.rhs.evaluate(binding)
+        return res
+    
+class Divide:
+    def __init__(self, lhs, rhs):
+        self.lhs = lhs
+        self.rhs = rhs
+        
+    def evaluate(self, binding):
+        res = self.lhs.evaluate(binding) / self.rhs.evaluate(binding)
+    
+class Plus:
+    def __init__(self, lhs, rhs):
+        self.lhs = lhs
+        self.rhs = rhs
+        
+    def evaluate(self, binding):
+        res = self.lhs.evaluate(binding) + self.rhs.evaluate(binding)
+        return res
+    
+class Minus:
+    def __init__(self, lhs, rhs):
+        self.lhs = lhs
+        self.rhs = rhs
+    
+    def evaluate(self, binding):
+        res = self.lhs.evaluate(binding) - self.rhs.evaluate(binding)
+        return res
+    
+class Equals:
+    def __init__(self, lhs, rhs):
+        self.lhs = lhs
+        self.rhs = rhs
+    
+    def evaluate(self, binding):
+        if(self.lhs.evaluate(binding) == self.rhs.evaluate(binding)):
+            return True
+        return False
+
+class NotEquals:
+    def __init__(self, lhs, rhs):
+        self.lhs = lhs
+        self.rhs = rhs
+    
+    def evaluate(self, binding):
+        if(self.lhs.evaluate(binding) == self.rhs.evaluate(binding)):
+            return False
+        return True
+
+class GreaterEquals:
+    def __init__(self, lhs, rhs):
+        self.lhs = lhs
+        self.rhs = rhs
+    
+    def evaluate(self, binding):
+        if(self.lhs.evaluate(binding) >= self.rhs.evaluate(binding)):
+            return True
+        return False
+    
+class Greater:
+    def __init__(self, lhs, rhs):
+        self.lhs = lhs
+        self.rhs = rhs
+    
+    def evaluate(self, binding):
+        if(self.lhs.evaluate(binding) > self.rhs.evaluate(binding)):
+            return True
+        return False
+    
+class LessEquals:
+    def __init__(self, lhs, rhs):
+        self.lhs = lhs
+        self.rhs = rhs
+    
+    def evaluate(self, binding):
+        if(self.lhs.evaluate(binding) <= self.rhs.evaluate(binding)):
+            return True
+        return False
+
+class Less:
+    def __init__(self, lhs, rhs):
+        self.lhs = lhs
+        self.rhs = rhs
+    
+    def evaluate(self, binding):
+        if(self.lhs.evaluate(binding) < self.rhs.evaluate(binding)):
+            return True
+        return False
+
+#############
+# TERMINALS #
+#############
+
+class Integer:
     def __init__(self, value):
         self.value = value
     
     def evaluate(self, binding):
         return self.value
+
+class Identifier:
+    def __init__(self, ident):
+        self.ident = ident
     
-    
+    def evaluate(self, binding):
+        return self.ident.evaluate(binding)
--- visitor.py
+++ visitor.py
@@ -1,10 +1,64 @@
 from arpeggio import PTNodeVisitor
-import tree_nodes as node
+from tree_nodes import *
+from grammar import *
 
 class Visitor(PTNodeVisitor):
     
     def visit_integer(self, node, children):
-        return node
+        return node.value
+
+    def visit_arithmetic_expression(self, node, children):
+        if(children[1] == '+'):
+            return (Plus(children[0], children[2]))
+        if(children[1] == '-'):
+            return (Minus(children[0], children[2]))
+
+    def visit_mult_term(self, node, children):
+        if(len(children) <= 1):
+            if(children[1] == '*'):
+                return (Times(children[0], children[2]))
+            if(children[1] == '/'):
+                return (Divide(children[0], children[2]))
+        
+
+    def visit_boolean_expression(self, node, children):
+        if(children[1] == '=='):
+            return (Equals(children[0], children[2]))
+        if(children[1] == '!='):
+            return (NotEquals(children[0], children[2]))
+        if(children[1] == '>='):
+            return (GreaterEquals(children[0], children[2]))
+        if(children[1] == '>'):
+            return (Greater(children[0], children[2]))
+        if(children[1] == '<='):
+            return (LessEquals(children[0], children[2]))
+        if(children[1] == '<'):
+            return (Less(children[0], children[2]))
+
+    def visit_identifier(self, node, children):
+        return children
+
+    def visit_decl(self, node, children):
+        if(len(children) > 1):
+            return (Declaration(children[0], children[2]))
+        else:
+            return (Identifier(children[0]))
+
     
-    def visit_expression(self, node, children):
-        return node
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
